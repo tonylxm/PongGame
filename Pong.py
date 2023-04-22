1e4d48c8dd547646bfc293b814817fde8ae0a695
Tony <tlim334@aucklanduni.ac.nz>
2023-04-22 20:49:49 +1200

    Score and scoreboard added. Next: add sound and gradually increasing speed of ball

--- Pong.py
+++ Pong.py
@@ -3,6 +3,7 @@
 # DATE LAST EDITED:22/04/2023
 
 # Pong Game
+import turtle
 from PongObject import PongObject, Screen
 
 # Create window
@@ -11,6 +12,10 @@ wn.title("PONG by Tony Lim")
 wn.bgcolor("black")
 wn.setup(width = 800, height = 600)
 wn.tracer(0)    # Stops window from updating -> manually update -> faster game
+
+# Score
+score_a = 0
+score_b = 0
 
 # Create paddles
 paddle_a = PongObject(0, "white", (-350, 0), 5, 1)
@@ -27,6 +32,15 @@ wn.onkeypress(paddle_b.down, "Down")
 ball = PongObject(0, "white", (0, 0), 1, 1)
 ball.dx = 0.1
 ball.dy = 0.1
+
+# Create scoreboard
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0      Player B: 0", align = "center", font = ("Courier", 24, "normal"))
 
 # Main game loop
 while True:
@@ -49,10 +63,16 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}      Player B: {}".format(score_a, score_b), align = "center", font = ("Courier", 24, "normal"))
     
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b +=1
+        pen.clear()
+        pen.write("Player A: {}      Player B: {}".format(score_a, score_b), align = "center", font = ("Courier", 24, "normal"))
 
     # Paddle and ball collisions
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
