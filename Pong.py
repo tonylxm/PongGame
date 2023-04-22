d804500f0cbef975c1d8b6080df2c95325e1d1ad
Tony <tlim334@aucklanduni.ac.nz>
2023-04-22 19:58:39 +1200

    Keyboard bindings to control paddles. Ball now moves and bounces off window borders. Next steps: play around with speed of ball and add paddles & balls collisions

--- Pong.py
+++ Pong.py
@@ -16,9 +16,40 @@ wn.tracer(0)    # Stops window from updating -> manually update -> faster game
 paddle_a = PongObject(0, "white", (-350, 0), 5, 1)
 paddle_b = PongObject(0, "white", (350, 0), 5, 1)
 
+# Keyboard bindings
+wn.listen()
+wn.onkeypress(paddle_a.up, "w")
+wn.onkeypress(paddle_a.down, "s")
+wn.onkeypress(paddle_b.up, "Up")
+wn.onkeypress(paddle_b.down, "Down")
+
 # Create ball
 ball = PongObject(0, "white", (0, 0), 1, 1)
+ball.dx = 0.1
+ball.dy = 0.1
 
 # Main game loop
 while True:
     wn.update() # Manually update window
+
+    # Move ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    
+    # Border check
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+    
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
